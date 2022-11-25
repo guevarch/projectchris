0c2dffd0adfb1e20d38e4c9c346a94f7e3416951
guevarch <107594143+guevarch@users.noreply.github.com>
2022-11-24 22:03:34 -0500

    Add

--- app.py
+++ app.py
@@ -3,12 +3,20 @@ import numpy as np
 import pandas as pd
 from prophet import Prophet
 
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
    
    return render_template("index.html")
+
+@app.route("/home")
+def home():
+   
+   return render_template("home.html")
+
 
 @app.route('/run', methods=['GET', 'POST'])
 def route():
@@ -42,7 +50,7 @@ def route():
         prediction = ""
     
 
-    return render_template("index.html", output = prediction)
+    return render_template("index.html", output = prediction, date=Date)
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=5000)
