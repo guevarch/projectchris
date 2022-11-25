73feddabf9d792d39ace5cdd15332030c9b2e522
guevarch <107594143+guevarch@users.noreply.github.com>
2022-11-24 20:10:03 -0500

    add

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 import numpy as np
-import requests
+import pandas as pd
 from prophet import Prophet
 
 app = Flask(__name__)
 
-@app.route("/index")
+@app.route("/")
 def index():
    
    return render_template("index.html")
 
 @app.route('/run', methods=['GET', 'POST'])
-def main():
-    
+def route():
+    print('Inside route')
     # If a form is submitted
-    if requests.method == "POST":
+    if request.method == "POST":
         
-        df = pd.read_csv("Resources/mldata.csv")
+        df = pd.read_csv("Resources/mldatapriceslog.csv")
 
         # instantiate the model and set parameters
         model = Prophet()
@@ -25,22 +25,24 @@ def main():
         model.fit(df)
 
         # # Get values through input bars
-        start = requests.form.get("start")
-        end = requests.form.get("end")    
+        start = "2010-09-25"
+        Date = request.form.get("Date")    
 
-        insample = pd.DataFrame(pd.date_range(start, end, periods=92))
+        insample = pd.DataFrame(pd.date_range(start, Date, periods=92))
         insample.columns = ['ds']
 
         # in-sample prediction
         prediction = model.predict(insample)
 
         # Get prediction
-        prediction = prediction[prediction['ds'].dt.strftime('%Y-%m-%d') == end]
+        prediction = prediction[prediction['ds'].dt.strftime('%Y-%m-%d') == Date]
         prediction = np.exp(prediction.yhat)
+        prediction = prediction.values[0].round(2)
     else:
         prediction = ""
-        
-    return redirect("/index", output = prediction)
+    
 
-if __name__ == '__main__':
-   app.run()
+    return render_template("index.html", output = prediction)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000)
